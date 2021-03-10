8eecd8436cec3494482aeb8b3b0ef8a39a6e4d46
Jelson J <jelson20@outlook.com>
2021-03-10 18:15:15 +0000

    add sass

---
+++ pages/courses/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../courses/course.module.scss'
+
+
+ const Index = () => {
+  return (
+    <div>
+    <h1 className={styles.red}>Courses <span>Computer Sience</span></h1>
+    </div>
+  )
+}
+
+export default Index 
