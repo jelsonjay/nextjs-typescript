b1eb50782b5fa3b85b1cdf60e746171385a7dcbb
Jelson J <jelson20@outlook.com>
2021-03-13 17:26:53 +0000

    commit add styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument from 'next/document'
+import {ServerStyleSheet} from 'styled-components'
 
-class MyDocument extends Document {
-
-  render() {
-
-    return (
-      <Html>
-        <Head />
-        <meta property='test' content='me' />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default class Document extends NextDocument {
+static async getStaticProps(ctx) {
+  
+const sheet = new ServerStyleSheet()
+const originalRenderPage = ctx.renderPage
+try{
+ctx.renderPage = () => originalRenderPage({
+enchanceApp: (App) => props => sheet.collectStyles(<App {...props} />)
+})
+const initialProps = await NextDocument.getInitialProps(ctx)
+return{
+...initialProps,
+styles:(
+<>
+{initialProps.styles}
+{sheet.getStyleElement()}
+</>
+)}
 }
-
-export default MyDocument
+}
+}
